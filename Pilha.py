f08fc713c2d73ff3741d3d73bf55613d0746acbb
Fernando Mottin Maschmann <fernando.maschmann@outlook.com>
2021-06-13 18:30:46 -0300

    trabalho finalizado

---
+++ Pilha.py
@@ -0,0 +1,40 @@
+from Node import Node
+
+# inserir na pilha
+# remover da pilha
+# observar o topo da pilha
+class Pilha():
+    def __init__(self):
+        self.top = None
+        self._size = 0
+
+    def push(self, elem):
+        #insere um elemento na pilha
+        node = Node(elem)
+        node.next = self.top
+        self.top = node
+        self._size = self._size + 1
+
+    def pop(self):
+        # remove o elemento do topo da pilha
+        if self._size > 0:
+            node = self.top
+            self.top = self.top.next
+            self._size = self._size - 1
+            return node.data
+        raise IndexError("A pilha está vazia")
+
+    def __len__(self):
+        """Retorna o tamanho da lista"""
+        return self._size
+
+    def __repr__(self):
+        r = ""
+        pointer = self.top
+        while(pointer):
+            r = r + str(pointer.data) + "\n"
+            pointer = pointer.next
+        return r
+
+    def __str__(self):
+        return self.__repr__()
---
+++ main.py
@@ -0,0 +1,17 @@
+from Pilha import Pilha
+
+pilha = Pilha()
+
+pilha.push(6)
+pilha.push(8)
+
+print(pilha)
+
+pilha.pop()
+
+print(pilha)
+pilha.push(23)
+
+print(pilha)
+
+print(len(pilha))
